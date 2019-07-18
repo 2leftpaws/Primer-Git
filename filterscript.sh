5af0b808f58a44f4314c2caf6ae3b8cf9533ec0c
2leftpaws <wlma37@gmail.com>
2019-07-18 11:21:19 -0600

    Actualizacion de Archivo

--- filterscript.sh
+++ filterscript.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
+#ESTADO="$1"
+#if [ $ESTADO == EnProceso ]
+#then
+#echo "Proyectos En Proceso:" 
+#echo "---------------------"
+#echo *EnProceso
+#ls -d *ep* -- quitar despues 
+#elif [ $ESTADO == Terminado ]
+#then
+#echo "Proyectos Terminados:"
+#echo "---------------------"
+#echo *Terminado
+#ls -d *te* -- quitar despues
+#fi
 ESTADO="$1"
-echo $ESTADO
-ULTIMASLETRAS=${ESTADO: -1}
-if [[ $ESTADO == *"ep"* ]]
-then
-echo "Proyectos En Proceso:" 
-ls -d *ep*
-elif [[] $ESTADO == *"te"* ]]
-echo "Proyectos Terminados:"
-then
-ls -d *te*
-fi
+filtro_archivos () {
+echo "Proyectos $1:" 
+echo "---------------------"
+ls -d *$1 
+}
+filtro_archivos $1
+#echo *$1
